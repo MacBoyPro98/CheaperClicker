134d2f447ee025bebc68e3cdd5009862ce66f821
Jacob Winters <jacobwinters@users.noreply.github.com>
2019-04-26 14:00:41 -0600

    Make /take-quiz SSE handler more resilient

--- static/take-quiz.js
+++ static/take-quiz.js
@@ -5,18 +5,31 @@ for (let input of document.getElementsByTagName('input')) {
 }
 
 const eventSource = new EventSource('new-questions');
-let oldScore = 0;
+let oldQuestion, oldAnswers = [], oldScore = 0;
 eventSource.addEventListener('message', (e) => {
 	const {question: {question, answers}, score} = JSON.parse(e.data);
-	if (oldScore !== score) {
-		confettiShower(document.querySelector(':checked + label'));
-		oldScore = score;
+	if (oldQuestion === question
+		&& oldAnswers[0] === answers[0]
+		&& oldAnswers[1] === answers[1]
+		&& oldAnswers[2] === answers[2]
+		&& oldAnswers[3] === answers[3]
+		&& oldScore === score) {
+		return;
 	}
+	const checkedAnswer = document.querySelector(':checked');
+	if (checkedAnswer && oldScore !== score) {
+		confettiShower(document.querySelector(':checked + label'));
+	}
+	oldQuestion = question;
+	oldAnswers = answers;
+	oldScore = score;
 	document.getElementById('question').textContent = question;
 	const labels = document.getElementsByTagName('label');
 	for (let i = 0; i < 4; i++) {
 		labels[i].textContent = answers[i];
 	}
-	document.querySelector(':checked').checked = false;
+	if (checkedAnswer) {
+		checkedAnswer.checked = false;
+	}
 	document.getElementById('score').textContent = score;
 });
